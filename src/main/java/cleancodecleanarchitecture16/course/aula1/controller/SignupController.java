fe5d4e46191f8e411255db9a6a3148cf685b836c
Wallacy <wallacyrezende@gmail.com>
2024-04-07 00:31:49 -0300

    feat: adding tags swagger in the endpoints

--- src/main/java/cleancodecleanarchitecture16/course/aula1/controller/SignupController.java
+++ src/main/java/cleancodecleanarchitecture16/course/aula1/controller/SignupController.java
@@ -4,6 +4,8 @@ import cleancodecleanarchitecture16.course.aula1.SignupResponse;
 import cleancodecleanarchitecture16.course.aula1.model.dto.AccountDTO;
 import cleancodecleanarchitecture16.course.aula1.model.exceptions.BusinessException;
 import cleancodecleanarchitecture16.course.aula1.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Signup Controller")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -19,6 +22,7 @@ public class SignupController {
 
     private final AccountService accountService;
 
+    @Operation(summary = "Create a new Account")
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody AccountDTO accountDTO) {
         try {
